b9d96e409313eafa5006d8ccef83e0662e6a8df6
andrew-s28 <ascherer97@gmail.com>
2023-02-14 10:23:29 -0800

    fixed __init__ imports

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+PyCOARE
+===============
+"""
+
+from __future__ import absolute_import
+
+from .coare3p5 import bulk
+from .coare3p5 import coare35vn
+from .coare3p5 import meteo
+from .coare3p5 import util
+
+from .coare3p6 import coare36vn_zrf_et
+from .coare3p6 import coare36vnWarm_et
