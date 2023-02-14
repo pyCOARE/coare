4033cf27ced0c5c3eaf43474ed69952f8e357630
andrew-s28 <ascherer97@gmail.com>
2023-02-14 14:31:39 -0800

    another restructuring

--- pycoare/__init__.py
+++ pycoare/__init__.py
@@ -7,10 +7,10 @@ PyCOARE
 
 from __future__ import absolute_import
 
-from .coare3p5 import bulk
-from .coare3p5 import coare35vn
-from .coare3p5 import meteo
-from .coare3p5 import util
+from . import bulk
+from . import coare35vn
+from . import meteo
+from . import util
 
-from .coare3p6 import coare36vn_zrf_et
-from .coare3p6 import coare36vnWarm_et
+from . import coare36vn_zrf_et
+from . import coare36vnWarm_et
