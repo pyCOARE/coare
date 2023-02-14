e02fa7ec22cc09d790fdc0d53c4dabddb263c4db
andrew-s28 <ascherer97@gmail.com>
2023-02-14 10:23:33 -0800

    modified:   src/coare3p5/__init__.py 	modified:   src/coare3p6/__init__.py

--- src/coare3p5/__init__.py
+++ src/coare3p5/__init__.py
@@ -7,4 +7,7 @@ PyCOARE
 
 from __future__ import absolute_import
 
+from . import bulk
+from . import coare35vn
+from . import meteo
 from . import util
--- src/coare3p6/__init__.py
+++ src/coare3p6/__init__.py
@@ -7,4 +7,5 @@ PyCOARE
 
 from __future__ import absolute_import
 
-from . import util
+from . import coare36vn_zrf_et
+from . import coare36vnWarm_et
