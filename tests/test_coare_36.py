359c9b9972ff53727bb09dfb0b00eac419c58245
Andrew Scherer <ascherer97@gmail.com>
2025-03-20 17:54:23 -0700

    adds tests for coare_36 and restructures testing library, drops 3.8 support, and adds tests for 3.12 and 3.13

---
+++ tests/test_coare_36.py
@@ -0,0 +1,236 @@
+"""
+Tests for the COARE functions in pycoare.coare_36
+"""
+
+import csv
+import os
+
+import numpy as np
+import pytest
+
+from pycoare import coare_36
+
+# required or tests fail since numpy doesn't save enough precision
+np.set_printoptions(precision=12)
+
+
+@pytest.fixture
+def load_input():
+    path = os.path.join(os.path.dirname(__file__), "data/c35_c36_test_input.csv")
+    data = list(csv.reader(open(path)))
+    input_data = {i[0]: np.array(i[1:], dtype=float) for i in zip(*data)}
+    input_data.update({"jcool": 1, "nits": 10})
+    return input_data
+
+
+@pytest.fixture
+def load_expected():
+    path = os.path.join(os.path.dirname(__file__), "data/c36_test_expected.csv")
+    data = list(csv.reader(open(path)))
+    expected = {i[0]: np.array(i[1:], dtype=float) for i in zip(*data)}
+    return expected
+
+
+class TestOutputC35:
+    def test_fluxes(self, load_input, load_expected):
+        expected = load_expected
+        actual = coare_36(**load_input).fluxes
+        np.testing.assert_allclose(actual.rnl, expected["rnl"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.tau, expected["tau"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.hsb, expected["hsb"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.hlb, expected["hlb"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.hbb, expected["hbb"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.hsbb, expected["hsbb"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(
+            actual.hlwebb, expected["hlwebb"], atol=1e-10, rtol=0
+        )
+        np.testing.assert_allclose(actual.evap, expected["evap"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.rf, expected["rf"], atol=1e-10, rtol=0)
+
+    def test_velocities(self, load_input, load_expected):
+        expected = load_expected
+        actual = coare_36(**load_input).velocities
+        np.testing.assert_allclose(actual.ut, expected["ut"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.usr, expected["usr"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.du, expected["du"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.gf, expected["gf"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.u, expected["u"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.u_rf, expected["u_rf"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.u_n, expected["u_n"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(
+            actual.u_n_rf, expected["u_n_rf"], atol=1e-10, rtol=0
+        )
+
+    def test_temperatures(self, load_input, load_expected):
+        expected = load_expected
+        actual = coare_36(**load_input).temperatures
+        np.testing.assert_allclose(actual.lapse, expected["lapse"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.dt, expected["dt"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.dter, expected["dter"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.t_rf, expected["t_rf"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.t_n, expected["t_n"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(
+            actual.t_n_rf, expected["t_n_rf"], atol=1e-10, rtol=0
+        )
+
+    def test_humidities(self, load_input, load_expected):
+        expected = load_expected
+        actual = coare_36(**load_input).humidities
+        np.testing.assert_allclose(actual.dq, expected["dq"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.dqer, expected["dqer"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.q_rf, expected["q_rf"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.q_n, expected["q_n"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(
+            actual.q_n_rf, expected["q_n_rf"], atol=1e-10, rtol=0
+        )
+        np.testing.assert_allclose(actual.rh_rf, expected["rh_rf"], atol=1e-10, rtol=0)
+
+    def test_stability_parameters(self, load_input, load_expected):
+        expected = load_expected
+        actual = coare_36(**load_input).stability_parameters
+        np.testing.assert_allclose(actual.tsr, expected["tsr"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.tvsr, expected["tvsr"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.tssr, expected["tssr"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.qsr, expected["qsr"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.tkt, expected["tkt"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.obukL, expected["obukL"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.zet, expected["zet"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.zo, expected["zo"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.zot, expected["zot"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.zoq, expected["zoq"], atol=1e-10, rtol=0)
+
+    def test_transfer_coefficients(self, load_input, load_expected):
+        expected = load_expected
+        actual = coare_36(**load_input).transfer_coefficients
+        np.testing.assert_allclose(actual.cd, expected["cd"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.ch, expected["ch"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(actual.ce, expected["ce"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(
+            actual.cdn_rf, expected["cdn_rf"], atol=1e-10, rtol=0
+        )
+        np.testing.assert_allclose(
+            actual.chn_rf, expected["chn_rf"], atol=1e-10, rtol=0
+        )
+        np.testing.assert_allclose(
+            actual.cen_rf, expected["cen_rf"], atol=1e-10, rtol=0
+        )
+
+    def test_stability_functions(self, load_input, load_expected):
+        expected = load_expected
+        actual = coare_36(**load_input).stability_functions
+        np.testing.assert_allclose(actual.psi_u, expected["psi_u"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(
+            actual.psi_u_rf, expected["psi_u_rf"], atol=1e-10, rtol=0
+        )
+        np.testing.assert_allclose(actual.psi_t, expected["psi_t"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(
+            actual.psi_t_rf, expected["psi_t_rf"], atol=1e-10, rtol=0
+        )
+        np.testing.assert_allclose(actual.psi_q, expected["psi_q"], atol=1e-10, rtol=0)
+        np.testing.assert_allclose(
+            actual.psi_q_rf, expected["psi_q_rf"], atol=1e-10, rtol=0
+        )
+
+
+class TestC35Attributes:
+    def test_fluxes(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35, "fluxes")
+
+    def test_velocities(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35, "velocities")
+
+    def test_temperatures(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35, "temperatures")
+
+    def test_humidities(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35, "humidities")
+
+    def test_stability_parameters(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35, "stability_parameters")
+
+    def test_transfer_coefficients(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35, "transfer_coefficients")
+
+    def test_stability_functions(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35, "stability_functions")
+
+
+class TestSubclassAttributes:
+    def test_fluxes(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35.fluxes, "rnl")
+        assert hasattr(c35.fluxes, "tau")
+        assert hasattr(c35.fluxes, "hsb")
+        assert hasattr(c35.fluxes, "hlb")
+        assert hasattr(c35.fluxes, "hbb")
+        assert hasattr(c35.fluxes, "hsbb")
+        assert hasattr(c35.fluxes, "hlwebb")
+        assert hasattr(c35.fluxes, "evap")
+        assert hasattr(c35.fluxes, "rf")
+
+    def test_velocities(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35.velocities, "ut")
+        assert hasattr(c35.velocities, "usr")
+        assert hasattr(c35.velocities, "du")
+        assert hasattr(c35.velocities, "gf")
+        assert hasattr(c35.velocities, "u")
+        assert hasattr(c35.velocities, "u_rf")
+        assert hasattr(c35.velocities, "u_n")
+        assert hasattr(c35.velocities, "u_n_rf")
+
+    def test_temperatures(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35.temperatures, "lapse")
+        assert hasattr(c35.temperatures, "dt")
+        assert hasattr(c35.temperatures, "dter")
+        assert hasattr(c35.temperatures, "t_rf")
+        assert hasattr(c35.temperatures, "t_n")
+        assert hasattr(c35.temperatures, "t_n_rf")
+
+    def test_humidities(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35.humidities, "dq")
+        assert hasattr(c35.humidities, "dqer")
+        assert hasattr(c35.humidities, "q_rf")
+        assert hasattr(c35.humidities, "q_n")
+        assert hasattr(c35.humidities, "q_n_rf")
+        assert hasattr(c35.humidities, "rh_rf")
+
+    def test_stability_parameters(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35.stability_parameters, "tsr")
+        assert hasattr(c35.stability_parameters, "tvsr")
+        assert hasattr(c35.stability_parameters, "tssr")
+        assert hasattr(c35.stability_parameters, "qsr")
+        assert hasattr(c35.stability_parameters, "tkt")
+        assert hasattr(c35.stability_parameters, "obukL")
+        assert hasattr(c35.stability_parameters, "zet")
+        assert hasattr(c35.stability_parameters, "zo")
+        assert hasattr(c35.stability_parameters, "zot")
+        assert hasattr(c35.stability_parameters, "zoq")
+
+    def test_transfer_coefficients(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35.transfer_coefficients, "cd")
+        assert hasattr(c35.transfer_coefficients, "ch")
+        assert hasattr(c35.transfer_coefficients, "ce")
+        assert hasattr(c35.transfer_coefficients, "cdn_rf")
+        assert hasattr(c35.transfer_coefficients, "chn_rf")
+        assert hasattr(c35.transfer_coefficients, "cen_rf")
+
+    def test_stability_functions(self, load_input):
+        c35 = coare_36(**load_input)
+        assert hasattr(c35.stability_functions, "psi_u")
+        assert hasattr(c35.stability_functions, "psi_u_rf")
+        assert hasattr(c35.stability_functions, "psi_t")
+        assert hasattr(c35.stability_functions, "psi_t_rf")
+        assert hasattr(c35.stability_functions, "psi_q")
+        assert hasattr(c35.stability_functions, "psi_q_rf")
---
+++ tests/test_utils.py
@@ -0,0 +1,125 @@
+"""
+Tests for the COARE functions in pycoare.utils
+"""
+
+import numpy as np
+import pytest
+
+from pycoare.util import psit_26, psiu_26, psiu_40, qair, qsat, qsea, rhcalc
+
+# required or tests fail since numpy doesn't save enough precision
+np.set_printoptions(precision=12)
+
+
+class TestUtil:
+    @pytest.fixture
+    def input_zet(self):
+        return dict(z_L=[-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
+
+    def test_psit_26(self, input_zet):
+        expected = np.array(
+            [
+                3.12724961201,
+                2.94563937069,
+                2.7153981664,
+                2.39730604321,
+                1.86548667371,
+                -0.0,
+                -4.43410797233,
+                -8.02103778406,
+                -11.0874415242,
+                -13.8543826804,
+                -16.469041132,
+            ]
+        )
+        actual = psit_26(**input_zet)
+        np.testing.assert_allclose(actual, expected, atol=1e-10, rtol=0)
+
+    def test_psiu_26(self, input_zet):
+        expected = np.array(
+            [
+                2.17508663882,
+                2.0118078783,
+                1.80735371734,
+                1.53234530616,
+                1.11049402203,
+                -0.0,
+                -4.39257224887,
+                -7.53860684364,
+                -9.85231262359,
+                -11.6119662782,
+                -13.0040743224,
+            ]
+        )
+        actual = psiu_26(**input_zet)
+        np.testing.assert_allclose(actual, expected, atol=1e-10, rtol=0)
+
+    def test_psiu_40(self, input_zet):
+        expected = np.array(
+            [
+                2.16931171714,
+                2.0088210855,
+                1.80959461827,
+                1.54608872607,
+                1.15293343263,
+                -0.0,
+                -4.69257224887,
+                -8.13860684364,
+                -10.7523126236,
+                -12.8119662782,
+                -14.5040743224,
+            ]
+        )
+        actual = psiu_40(**input_zet)
+        np.testing.assert_allclose(actual, expected, atol=1e-10, rtol=0)
+
+    @pytest.fixture()
+    def input_rhcalc(self):
+        return dict(
+            t=[0, 1, 2, 3, 4],
+            p=[1006, 1008, 1010, 1012, 1014],
+            q=[0.001, 0.002, 0.003, 0.004, 0.005],
+        )
+
+    def test_rhcalc(self, input_rhcalc):
+        expected = np.array(
+            [26.335489852, 49.0630103047, 68.5938644747, 85.2938428107, 99.4883206669]
+        )
+        actual = rhcalc(**input_rhcalc)
+        np.testing.assert_allclose(actual, expected, atol=1e-10, rtol=0)
+
+    @pytest.fixture()
+    def input_qsea(self):
+        return dict(
+            t=[0, 1, 2, 3, 4],
+            p=[1006, 1008, 1010, 1012, 1014],
+        )
+
+    def test_qsat(self, input_qsea):
+        expected = np.array(
+            [6.1376532232, 6.59809274866, 7.08885199804, 7.61163590591, 8.1682276771]
+        )
+        actual = qsat(**input_qsea)
+        np.testing.assert_allclose(actual, expected, atol=1e-10, rtol=0)
+
+    def test_qsea(self, input_qsea):
+        expected = np.array(
+            [3.72737831642, 3.99971180693, 4.28945019389, 4.59754176056, 4.92497736469]
+        )
+        actual = qsea(**input_qsea)
+        np.testing.assert_allclose(actual, expected, atol=1e-10, rtol=0)
+
+    @pytest.fixture()
+    def input_qair(self):
+        return dict(
+            t=[0, 1, 2, 3, 4],
+            p=[1006, 1008, 1010, 1012, 1014],
+            rh=[65, 70, 75, 80, 85],
+        )
+
+    def test_qair(self, input_qair):
+        expected = np.array(
+            [2.47023726098, 2.85481658691, 3.28057712818, 3.7509893477, 4.26976278961]
+        )
+        actual = qair(**input_qair)
+        np.testing.assert_allclose(actual, expected, atol=1e-10, rtol=0)
